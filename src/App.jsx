e417b8af59a37580a84d4d7e6f7504f53471dc3d
Karan-Bharti1 <karan.codes1@gmail.com>
2025-03-26 12:56:48 +0530

    feat: created useFetch hook to pass authorisation header

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,28 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [secret, setSecret] = useState('')
+const handleClick=async()=>{
+const response=await fetch("https://authentication-basics.vercel.app/admin/login",{
+  method:'POST',
+  headers:{
+    'Content-Type':'application/json'
+  },
+  body:JSON.stringify({secret})
+})
+const data=await response.json()
+console.log(data)
+localStorage.setItem('token',data.token)
+
+}
 
   return (
     <>
+    <div>
+      <input 
+      value={secret} onChange={e=>setSecret(e.target.value)}/>
+      <button onClick={handleClick}>Login</button>
+    </div>
     </>
   )
 }
